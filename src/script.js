711f602da8bb8e5856be45385d75f90d4c06e454
Tomas <53576581+tomaschapot@users.noreply.github.com>
2022-02-21 18:54:51 -0300

    added board interaction and support functions
    Added the functions to show the card color you clicked

--- src/script.js
+++ src/script.js
@@ -16,13 +16,33 @@ function gameConfig() {
 		});
 	}
 
+	function hideColors() {
+		$cards.forEach((card) => {
+			card.classList.add("hidden-color");
+		});
+	}
+
 	assignColor();
+	hideColors();
 }
 
 gameConfig();
+elementInteraction($board);
 
-//Shuffle Array
+//Support Functions
 
+function elementInteraction(board) {
+	board.onclick = function (e) {
+		const $elemento = e.target; // Selects the element clicked.
+		if ($elemento.classList.contains("card")) {
+			cardHandler($elemento);
+		}
+	};
+}
+
+function cardHandler(currentCard) {
+	showCard(currentCard);
+}
 function arrayShuffle(array) {
 	let currentIndex = array.length;
 	let randomIndex;
@@ -39,9 +59,11 @@ function arrayShuffle(array) {
 
 	return array;
 }
+function showCard(card) {
+	card.classList.remove("hidden-color");
+}
 
-/* 1.Creo el color, se lo asigno como clase a dos divs, eso hasta completar los 12 divs
-2. Creo una funcion que cada vez que se inicia el juego se vuelve a asignar los colores a los divs.
+/*
 3. Una vez que tengo esta base, tengo que hacer que si el target que estoy senialado tiene la misma clase que el otro target que senialo se selecciona bien, sino
 se sigue seleccionando.
 4. Una vez que seleccione todas, ejecutar un mensaje de que ganaste.
